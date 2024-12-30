e1df514347e84fb4806b9e2507c50992851d0546
K <kasikorntripforwork@gmail.com>
2024-12-30 21:45:55 +0700

    feat: try express

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-fs.writeFile('example.txt', 'Hello, Node.js!', (err) => {
-  if (err) throw err;
-  console.log('File created!');
+app.get('/', (req, res) => {
+  res.send('Hello, Express.js!');
+});
+
+app.get('/users', (req, res) => {
+  res.json([
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Jane' },
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
